d86e5c41170ab8bb2707361bfcb499c0a3622405
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-14 18:30:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+def countdown(number)
+  countdown = 10
+  say = "HAPPY NEW YEAR!"
+  while countdown > 0 do
+  puts "#{countdown} SECOND(S)!" 
+  countdown -= 1
+  end
+  return say
+end
 
-def countdown
+def countdown_with_sleep(runtime)
+  while runtime.real.to_i >=5
+  end
 end
