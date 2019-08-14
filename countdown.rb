4bedf0e996c9b93bc83ae112191fdfa408728a9b
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-14 18:45:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,11 +12,5 @@ end
 def countdown_with_sleep(number)
   sleep 5
   countdown = 10
-  say = "HAPPY NEW YEAR!"
-  while countdown > 0 do
-  puts "#{countdown} SECOND(S)!" 
-  countdown -= 1
-  end
-  return say
-  end
+  
 end
